de45d0862f398930e9b3740ed3c32742ab1db738
adithyavenkatesh <adithyav211@gmail.com>
2020-10-05 17:56:08 -0400

    fixed bar graph

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,27 +1,23 @@
-var jsData;
 var select = d3.select('select');
 var panel = d3.select('.panel-body');
-
+var jsData;
 d3.json('../data/samples.json').then(data => {
+    jsData = data;
     var names = data.names;
-    
     names.forEach(name => {
         select.append('option').text(name).property('value',name)
     });
     
     showDemo(names[0])
     showBubbles(names[0])
-
-
-    jsData = data;
-    console.log(data.samples);
+    showPlot(names[0])
 });
 
 function showDemo(name) {
     d3.json('../data/samples.json').then( data => {
         var metadata = data.metadata.filter(obj => obj.id == name)[0];
         Object.entries(metadata).forEach(([key,value]) => {
-            panel.append('h6').text(`${key.toUpperCase()}: ${value}`)
+            panel.append('h5').text(`${key.toUpperCase()}: ${value}`)
         });
     });
 };
@@ -42,10 +38,30 @@ function showBubbles(name) {
             }
         };
 
-        
-        console.log(bubbleData);
-        
-        Plotly.newPlot('bubble', bubbleData)
+        var data = [bubbleData];
+
+        Plotly.newPlot('bubble', data);
         
     });
 };
+
+function showPlot(name) {
+    d3.json('../data/samples.json').then(data => {
+        var sample = data.samples.filter(obj => obj.id == name)[0];
+
+        var barPlot = {
+            x: sample.sample_values.slice(0,10).reverse(),
+            y: sample.otu_ids.slice(0,10).reverse().map(otuID => `OTU ${otuID}`),
+            text: sample.otu_labels.slice(0, 10).reverse(),
+            type:"bar",
+            orientation: "h",
+        };
+
+        var data = [barPlot];
+
+        Plotly.newPlot("bar", data);
+        
+    });
+
+};
+
